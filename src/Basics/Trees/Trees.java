1e528340a37ef422cab8992d0502ea0e5fa530c6
anirudhcheeti <anirudhcheeti.dev@gmail.com>
2026-07-13 09:34:26 +0530

    Trees Count

--- src/Basics/Trees/Trees.java
+++ src/Basics/Trees/Trees.java
@@ -17,49 +17,64 @@ public class Trees {
     public Trees() {
         root = null;
     }
-    public void insert(int val) {
-        root=insertrecursive(root, val);
-    }
-    private TreeNode insertrecursive(TreeNode root, int val) {
-        if (root == null) {
-            return new TreeNode(val);
-        }
-        if(root.val < val) {
-            root.right=insertrecursive(root.right, val);
-        }
-        else if(root.val > val) {
-            root.left=insertrecursive(root.left, val);
-        }
-        return root;
-    }
 
-    public void display(){
-        displaytree(root);
-        System.out.println();
-    }
+        //Insert in the TREE
+        public void insert(int val) {
+            root=insertrecursive(root, val);
+        }
+        private TreeNode insertrecursive(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+            if(root.val < val) {
+                root.right=insertrecursive(root.right, val);
+            }
+            else if(root.val > val) {
+                root.left=insertrecursive(root.left, val);
+            }
+            return root;
+        }
 
-    private void displaytree(TreeNode root){
+        //Display the TREE
+        public void display(){
+            displaytree(root);
+            System.out.println();
+        }
+
+        private void displaytree(TreeNode root){
+            if(root==null){
+                return;
+            }
+            displaytree(root.left);
+            System.out.print(root.val + " ");
+            displaytree(root.right);
+        }
+
+
+        //Search in a TREE
+        public boolean search(int val){
+            return searchrecursive(root, val);
+        }
+        private boolean searchrecursive(TreeNode root, int val){
+            if(root==null){
+                return false;
+            }
+            if(root.val == val){
+                return true;
+            }
+            if(root.val<val) {
+                return searchrecursive(root.right, val);
+            }
+                return searchrecursive(root.left, val);
+        }
+
+        public int count(){
+            return countrecursive(root);
+        }
+        private int countrecursive(TreeNode root){
         if(root==null){
-            return;
+            return 0;
         }
-        displaytree(root.left);
-        System.out.print(root.val + " ");
-        displaytree(root.right);
-    }
-
-    public boolean search(int val){
-        return searchrecursive(root, val);
-    }
-    private boolean searchrecursive(TreeNode root, int val){
-        if(root==null){
-            return false;
+        return countrecursive(root.left) + countrecursive(root.right) + 1;
         }
-        if(root.val == val){
-            return true;
-        }
-        if(root.val<val) {
-            return searchrecursive(root.right, val);
-        }
-            return searchrecursive(root.left, val);
-    }
 }
--- src/Basics/Trees/main.java
+++ src/Basics/Trees/main.java
@@ -3,14 +3,16 @@ package Basics.Trees;
 public class main {
     public static void main(String[] args) {
         Trees t=new Trees();
-        t.insert(8);
-        t.insert(5);
-        t.insert(10);
-        t.insert(6);
-        t.insert(9);
-        t.insert(0);
         t.insert(1);
         t.insert(2);
-        System.out.println("Is 9 in the tree? " + t.search(11));
+        t.insert(3);
+        t.insert(4);
+        t.insert(5);
+        t.insert(6);
+        t.insert(7);
+        t.insert(8);
+
+        t.count();
+        System.out.println(t.count());
     }
 }
